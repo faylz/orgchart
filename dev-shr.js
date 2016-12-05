8bfd0be5df20ac65829863f67558d7c989cc0f1d
lizheng <619806081@qq.com>
2016-12-05 23:57:35 +0800

    commit

--- dev-shr.js
+++ dev-shr.js
@@ -55,51 +55,57 @@
 					
 				},
 				$(go.Panel, "Table",
-				{
-					defaultAlignment: go.Spot.Left
-				},
-				$(go.RowColumnDefinition, { column: 1 }),
-				$(go.Picture,
 					{
-					  name: 'Picture',
-					  desiredSize: new go.Size(55, 75),
-					  margin: new go.Margin(6, 8, 6, 10),
-					  row: 0, column: 0, columnSpan: 5
+						defaultAlignment: go.Spot.Left
 					},
-					new go.Binding("source", "key", findHeadShot))),
+					$(go.RowColumnDefinition, { column: 1 }),
+					$(go.Picture,
+						{
+						  name: 'Picture',
+						  desiredSize: new go.Size(60, 75),
+						  margin: new go.Margin(5, 5, 5, 5),
+						  row: 0, column: 0, columnSpan: 5
+						},
+						new go.Binding("source", "key", findHeadShot))),//end table
 				$(go.Panel, "Table",
 					{
-						desiredSize:new go.Size(15,75),
-						defaultAlignment: go.Spot.Right
+						desiredSize:new go.Size(15,10),
+						defaultAlignment: go.Spot.Center
 					},
 					$(go.RowColumnDefinition, { column: 1}),
-					$(go.Panel,  // this is underneath the "BODY"
-					{ height: 15 },  // always this height, even if the TreeExpanderButton is not visible
-					$("TreeExpanderButton")
-				 ))
+					$(go.Panel,
+						{ height: 10 },  // always this height, even if the TreeExpanderButton is not visible
+						$("TreeExpanderButton"))) // end Table Panel
 			), 
-			 
-			
 			  
 			  
-			  
-			  // define the panel where the text will appear
-			  $(go.Panel, "Table",
+				// define the panel where the text will appear
+		       $(go.Panel, "Table",
 				{
-				  maxSize: new go.Size(75, 150),
-				  margin: new go.Margin(5, 0, 5, 0),
+				  maxSize: new go.Size(120, 999),
+				  margin: new go.Margin(5, 10, 5, 10),
 				  defaultAlignment: go.Spot.Left
 				},
 				$(go.RowColumnDefinition, { column: 1, width: 4 }),
 				$(go.TextBlock, managerNameStyle(),  // the name
 				  {
 					row: 0, column: 0, columnSpan: 5,
-					isMultiline: false,
+					isMultiline: true,
 					minSize: new go.Size(10, 16),
 					alignment: go.Spot.Left
 				  },
-				  new go.Binding("text", "name").makeTwoWay()))  // end Table Panel
-			) // end Horizontal Panel
+				  new go.Binding("text", "name").makeTwoWay())),  // end Table Panel
+				 
+				$(go.Shape, "LineH",
+				  {
+					name: "SHAPE",  // named so that the above properties can refer to this GraphObject
+					width: 100, height: 1,
+					stroke: "gray",
+					fill: null,
+					strokeWidth: 1,
+					cursor: "pointer"
+				  })// end Table Panel
+			) // end Vertical Panel
 		
 		);//end node
 			
